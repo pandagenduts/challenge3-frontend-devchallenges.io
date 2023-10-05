416f5435c399ed7e2a77cf305f6e336d0a60d945
Donny Rendi <donny.rendi@yahoo.co.id>
2023-10-05 17:01:08 +0700

    before creating redux for content filter from header

--- src/components/layouts/Button.jsx
+++ src/components/layouts/Button.jsx
@@ -2,9 +2,9 @@ const Button = (props) => {
   const { showMenuHandler, children, className } = props;
 
   return (
-    <button onClick={showMenuHandler} className={`flex gap-[11px] ${className}`}>
+    <button onClick={showMenuHandler} className={`flex gap-[11px] bg-[#EA6565] text-white py-[15px] px-6 rounded-2xl cursor-pointer ${className}`}>
       <span
-        className="material-symbols-outlined cursor-pointer"
+        className="material-symbols-outlined"
       >
         search
       </span>
--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import ReactDOM, { useState } from "react";
+import { useState } from "react";
 import logo from "../../../assets/logo.png";
 import Button from "../Button";
 import Input from "../Input";
@@ -21,7 +21,7 @@ const Header = () => {
             actionOnClick={handlerShowMenu}
             readOnly
             placeholder="Add Location"
-            className="w-full bg-slate-300 cursor-pointer"
+            className="w-full cursor-pointer bg-slate-300"
           />
         </div>
         <div>
@@ -33,45 +33,48 @@ const Header = () => {
           />
         </div>
         <span
-          className="material-symbols-outlined cursor-pointer"
+          className="cursor-pointer material-symbols-outlined"
           onClick={handlerShowMenu}
         >
           search
         </span>
       </div>
       {showMenu && (
-        <menu className="fixed top-0 left-0 w-screen h-screen flex flex-col">
+        <menu className="fixed top-0 left-0 flex flex-col w-screen h-screen">
           <div
             id="menu-overlay"
             className="fixed top-0 left-0 w-screen h-screen backdrop-blur-[2px] z-10"
             onClick={handlerShowMenu}
           />
-          <div className="z-20 px-3 pt-[18px] pb-6 bg-white min-h-[632px]">
-            <div id="popup-menu-header" className="flex justify-between mb-4">
-              <p>Edit your search</p>
-              <span className="material-symbols-outlined cursor-pointer">
-                close
-              </span>
-            </div>
-            <nav className="">
-              <div className="flex flex-col sm:flex-row shadow-bsOne rounded-[16px] py-[11px] px-[26px]">
-                <div className="flex flex-col">
-                  <label htmlFor="location">LOCATION</label>
-                  <Input
-                    type="text"
-                    placeholder="Add Location"
-                    name="location"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="guest">GUESTS</label>
-                  <Input type="text" placeholder="Add guest" name="guests" />
-                </div>
-                <Button className="hidden sm:inline-block">Search</Button>
+          <form className="z-20 px-3 pt-[18px] pb-6 bg-white min-h-[632px] flex flex-col justify-between">
+            <div>
+              <div id="popup-menu-header" className="flex items-center justify-between mb-4">
+                <p className="text-xs font-bold font-mulish">Edit your search</p>
+                <span className="cursor-pointer material-symbols-outlined">
+                  close
+                </span>
               </div>
-              <div className="grid"></div>
-            </nav>
-          </div>
+              <nav className="">
+                <div className="flex flex-col sm:flex-row shadow-bsOne rounded-[16px] py-[11px] px-[26px]">
+                  <div className="flex flex-col">
+                    <label htmlFor="location">LOCATION</label>
+                    <Input
+                      type="text"
+                      placeholder="Add Location"
+                      name="location"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="guest">GUESTS</label>
+                    <Input type="text" placeholder="Add guest" name="guests" />
+                  </div>
+                  <Button className="hidden sm:inline-block">Search</Button>
+                </div>
+                <div className="grid"></div>
+              </nav>
+            </div>
+            <Button className='self-center'>Search</Button>
+          </form>
         </menu>
       )}
     </header>
