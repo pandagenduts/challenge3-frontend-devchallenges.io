c9bf90a87f6fc9cb7bbe06e8b0f9ee427363690c
Donny Rendi <donny.rendi@yahoo.co.id>
2023-10-09 06:01:51 +0700

    footer done; search algorithm on progress

--- src/components/layouts/Footer/Footer.jsx
+++ src/components/layouts/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 const Footer = () => {
   return (
-    <footer>
-      <h1>footer</h1>
+    <footer className="mt-24 font-montserrat text-[#828282] text-sm text-center">
+      <p>
+        created by <a href="https://github.com/pandagenduts/challenge3-frontend-devchallenges.io" target="_blank" rel="noopener noreferrer" className='font-bold'>
+          pandagenduts
+        </a>
+        - <a href="http://legacy.devchallenges.io" target="_blank" rel="noopener noreferrer">legacy.devChallenges.io</a> 
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
